26a7a17fc8134f750440e7d8ad30e0d3d2b6ca23
Mark Schenkel <mark.schenkel123@gmail.com>
2021-01-08 03:37:15 +0100

    test

--- js/main.js
+++ js/main.js
@@ -4,25 +4,25 @@ function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition, showError);
   } else {
-    x.innerText = "Geolocation is not supported by this browser.";
+    x.textContent = "Geolocation is not supported by this browser.";
   }
 }
 
 function showPosition(position) {
-  x.innerText = "Latitude: " + position.coords.latitude +
+  x.textContent = "Latitude: " + position.coords.latitude +
     "Longitude: " + position.coords.longitude;
 }
 
 function showError(error) {
   switch(error.code) {
     case error.PERMISSION_DENIED:
-      x.innterText= "User denied the request for Geolocation."
+      x.textContent= "User denied the request for Geolocation."
       break;
     case error.POSITION_UNAVAILABLE:
-      x.innerText = "Location information is unavailable."
+      x.textContent = "Location information is unavailable."
       break;
     case error.TIMEOUT:
-      x.innerText = "The request to get user location timed out."
+      x.textContent = "The request to get user location timed out."
       break;
   }
 }

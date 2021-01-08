6d23ba27e63f7dac4453acd9e613615ba917ee11
Mark Schenkel <mark.schenkel123@gmail.com>
2021-01-08 19:13:05 +0100

    added radius param

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ let longitude
 let latitude
 let randomLongitude
 let randomLatitude
+//radius in miles
+const searchRadius = 5000
+const minPopulation = 2000000
 
 
 function getLocation() {
@@ -90,8 +93,8 @@ function getRandomCityForQuiz() {
   //gen coordinates in ISO-6709
   randomLongitude = addLeadingZeroesAndSignLongitude(generateNewLongitudeCoordinate())
   randomLatitude = addLeadingZeroesAndSignLatitude(generateNewLatitudeCoordinate())
-  
-  let cityRequest = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?minPopulation=2000000&location=${randomLatitude}${randomLongitude}`
+
+  let cityRequest = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?radius=${searchRadius}&minPopulation=${minPopulation}&location=${randomLatitude}${randomLongitude}`
   console.log(cityRequest)
 
   //fetch city with random coordinates and over 2000000 inhabitants.(lat before long)
@@ -109,6 +112,24 @@ function getRandomCityForQuiz() {
       console.error(err);
     });
 
+}
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+  const R = 6371; // Radius of the earth in km
+  const dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  const dLon = deg2rad(lon2 - lon1);
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+  ;
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI/180)
 }
 
 
